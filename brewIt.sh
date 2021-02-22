85360864bc97457a56954a2a87bbadb3f683205e
Adam McLain <atommclain@gmail.com>
2021-02-22 09:45:25 -0500

    Updates for brewIt.sh

--- brewIt.sh
+++ brewIt.sh
@@ -3,5 +3,13 @@
 brew bundle
 
 # after install settings
+
+# fix quicklook
 xattr -cr ~/Library/QuickLook/*
+qlmanage -r
+qlmanage -r cache
+killall Finder
+# turn on line numbers for qlcolorcode
+defaults write org.n8gray.QLColorCode extraHLFlags '-l -W'
+
 duti dutiSettings
