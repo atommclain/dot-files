5c5c11e0c34cfc7a34222f420c7cca5ba39a07aa
Adam McLain <atommclain@gmail.com>
2021-03-02 09:50:12 -0500

    Move library to script

---
+++ macExport.sh
@@ -0,0 +1,26 @@
+# !/bin/bash
+# Run on source mac to prepare ~/Library items for migrating
+
+mkdir ~/Desktop/Library
+pushd .
+cd ~/Desktop/Library
+
+items=(Scripts Services Colors ColorPickers QuickLook "Keyboard Layouts" StickiesDatabase)
+for item in "${items[@]}"
+do
+    cp -r ~/Library/"$item" .
+done
+
+mkdir ~/Desktop/Library/Application\ Support
+items=("Little Snitch" Charles)
+for item in "${items[@]}"
+do
+    cp -r ~/Library/Application\ Support/"$item" .
+done
+
+mkdir -p ~/Desktop/Library/Developer/Xcode
+cp -r  ~/Library/Developer/Xcode/UserData ./Developer/Xcode
+
+mkdir ~/Desktop/Library/Preferences
+cp ~/Library/Preferences/com.apple.dt* ./Preferences
+popd
