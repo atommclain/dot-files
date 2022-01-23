35f2f23e575001a735079dcd538faf5ffe6a0d79
Adam McLain <atommclain@gmail.com>
2022-01-23 08:42:19 -0500

    update ish.sh

--- ish.sh
+++ ish.sh
@@ -1,11 +1,12 @@
-# /bin/sh
+#!/bin/sh
 
-echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/" > /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community/" >> /etc/apk/repositories
+##echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/" > /etc/apk/repositories
+##echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community/" >> /etc/apk/repositories
+#echo https://dl-cdn.alpinelinux.org/alpine/v3.12/main >> /etc/apk/repositories
+#echo https://dl-cdn.alpinelinux.org/alpine/v3.12/community >> /etc/apk/repositories
 apk update
 apk upgrade
-apk add git
-apk add vim
-apk add curl
+apk add git vim curl
+apk add make gcc build-base abuild binutils util-linux npm
 # fix concurrency issue
 git config --global pack.threads "1"
