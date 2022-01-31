e478600df10f4c50811ee042a127c2c83bca5c71
Adam McLain <atommclain@gmail.com>
2022-01-31 10:20:11 -0500

    Install homebrew if its not installed already Update homebrew and upgrade packages each time

--- brewIt.sh
+++ brewIt.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+command -v brew >/dev/null 2>&1 || {
+    echo >&2 "brew command not available, running instalation script"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
 brew analytics off
+# update homebrew
+brew update
+# upgrade installed packages
+brew upgrade
+# install all entries in Brewfile
 brew bundle
 
 # after install settings
