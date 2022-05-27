aac98203e01eaf08306f4c259bc36747c33d7f48
Adam McLain <atommclain@gmail.com>
2022-05-27 15:12:48 -0400

    Update vimSetup

--- vimSetup.sh
+++ vimSetup.sh
@@ -17,18 +17,18 @@ curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing bundles"
 mkdir -p $HOME/.vim/bundle
 cd $HOME/.vim/bundle
-$GIT_CLONE_PULL git://github.com/altercation/vim-colors-solarized.git
-$GIT_CLONE_PULL git://github.com/arcticicestudio/nord-vim.git
-$GIT_CLONE_PULL git://github.com/shaunsingh/nord.nvim.git
-$GIT_CLONE_PULL git://github.com/vim-airline/vim-airline.git
-$GIT_CLONE_PULL git://github.com/vim-airline/vim-airline-themes.git
-$GIT_CLONE_PULL git://github.com/vim-scripts/a.vim.git
-$GIT_CLONE_PULL git://github.com/can3p/incbool.vim.git
+$GIT_CLONE_PULL https://github.com/altercation/vim-colors-solarized.git
+$GIT_CLONE_PULL https://github.com/arcticicestudio/nord-vim.git
+$GIT_CLONE_PULL https://github.com/shaunsingh/nord.nvim.git
+$GIT_CLONE_PULL https://github.com/vim-airline/vim-airline.git
+$GIT_CLONE_PULL https://github.com/vim-airline/vim-airline-themes.git
+$GIT_CLONE_PULL https://github.com/vim-scripts/a.vim.git
+$GIT_CLONE_PULL https://github.com/can3p/incbool.vim.git
 $GIT_CLONE_PULL https://github.com/tpope/vim-fugitive.git
-$GIT_CLONE_PULL git://github.com/christoomey/vim-tmux-navigator.git
+$GIT_CLONE_PULL https://github.com/christoomey/vim-tmux-navigator.git
 $GIT_CLONE_PULL https://github.com/t9md/vim-quickhl
 vim -u NONE -c "helptags fugitive/doc" -c q
-$GIT_CLONE_PULL git@github.com:markabe/vim-jira-open.git
+$GIT_CLONE_PULL https://github.com/markabe/vim-jira-open.git
 
 mkdir -p $HOME/.vim/undodir
 mkdir -p $HOME/.tmp
