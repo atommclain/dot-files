b00f7dc77d1ad0e59c55b5b4bfa9366d8a3cfeb8
Adam McLain <atommclain@gmail.com>
2024-04-19 11:24:07 -0400

    Add macConfigFinerDock.sh

---
+++ macConfigFinerDock.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# configure finder sidebar and dock
+
+# Configure the finder sidebar
+if hash mysides 2>/dev/null; then
+    mysides remove all
+    mysides add Downloads file:///Users/$(whoami)/Downloads/
+    mysides add $(whoami) file:///Users/$(whoami)/
+    mysides add git file:///Users/$(whoami)/git/
+    mysides add Applications file:///Applications/
+else
+    echo "mysides is not installed"
+fi
+
+# This currently uses a localy built version via https://github.com/Kyle-Ye/dockutil/tree/spm
+# Need to fix this once the brew version works. See https://github.com/kcrawford/dockutil/pull/131
+if hash dockutil 2>/dev/null; then
+    dockutil --remove com.apple.launchpad.launcher --no-restart
+    dockutil --remove com.apple.MobileSMS --no-restart
+    dockutil --remove com.apple.mail --no-restart
+    dockutil --remove com.apple.Maps --no-restart
+    dockutil --remove com.apple.Photos --no-restart
+    dockutil --remove com.apple.FaceTime --no-restart
+    dockutil --remove com.apple.iCal --no-restart
+    dockutil --remove com.apple.AddressBook --no-restart
+    dockutil --remove com.apple.reminders --no-restart
+    dockutil --remove com.apple.Notes --no-restart
+    dockutil --remove com.apple.TV --no-restart
+    dockutil --remove com.apple.Music --no-restart
+    dockutil --remove com.apple.podcasts --no-restart
+    dockutil --remove com.apple.news --no-restart
+    dockutil --remove com.apple.AppStore --no-restart
+    dockutil --remove com.apple.systempreferences --no-restart
+
+    killall Dock
+else
+    echo "dockutil is not installed"
+fi
